8843108ccbee796b1dca98a66aaa956dc23ca71e
ilustra <kaiqueglock@gmail.com>
2021-10-23 18:11:42 -0300

    primeiro-commit

---
+++ App.js
@@ -0,0 +1,20 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Routes from './src/routs'
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
+}
+
+export default App;
